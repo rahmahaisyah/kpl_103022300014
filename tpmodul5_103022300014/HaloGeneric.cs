b481b6b6813ad4e0825a2d49caaf09a1129b50f1
rahmahaisyah <rahmahasyh@example.com>
2025-03-17 04:16:19 +0700

    Add project files.

---
+++ tpmodul5_103022300014/HaloGeneric.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_103022300014
+{
+    class HaloGeneric
+    {
+        public void SapaUser<T>(T user)
+        {
+            Console.WriteLine($"Halo user {user}");
+        }
+    }
+}
---
+++ tpmodul5_103022300014/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace tpmodul5_103022300014
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
