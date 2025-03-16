4609e426bd9e4568b516530e09dffe985e057fd2
rahmahaisyah <rahmahasyh@example.com>
2025-03-17 04:33:44 +0700

    “menambahkan class DataGeneric”

---
+++ tpmodul5_103022300014/DataGeneric.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul5_103022300014
+{
+    class DataGeneric<T>
+    {
+        private T data;
+        public DataGeneric(T data)
+        {
+            this.data = data;
+        }
+
+        public void PrintData()
+        {
+            Console.WriteLine("Data yang tersimpan adalah: {data}");
+        }
+    }
+}
